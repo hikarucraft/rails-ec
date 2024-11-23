e1e77d14cfcdeab560860c77ed873c7f36dcbcb6
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-11-23 23:17:20 +0900

    feat: ran command `rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_08_235334) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_23_141209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_08_235334) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
