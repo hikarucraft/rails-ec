b48a3545f9c433e56af540f1f9023cceb9df4cbc
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-11-23 22:05:51 +0900

    feat: add validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_name_no_precense){FactoryBot.build(:user,name:"   ")}
+  let(:user_email_no_precense){FactoryBot.build(:user,email:"   ")}
+  describe 'precense' do
+    it 'name is invalid' do
+      expect(user_name_no_precense).not_to be_valid
+    end
+    it 'email is invalid' do
+      expect(user_email_no_precense).not_to be_valid
+    end
+    
+  end
 end
